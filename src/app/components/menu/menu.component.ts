97b75213a596ee9b7d3f72d0ce529d471dc8b285
Sandra13988 <sandra13988@gmail.com>
2025-02-13 10:16:09 +0100

    feat(root): Create routes

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { RegulationsComponent } from '../regulations/regulations.component';
+import { CreditsComponent } from '../credits/credits.component';
+import { LenguageComponent } from '../lenguage/lenguage.component';
+import { AlertsComponent } from '../alerts/alerts.component';
 
 @Component({
   selector: 'app-menu',
