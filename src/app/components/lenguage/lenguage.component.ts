fddd4105e703ef37fe46fe99ff5c6eeeff2fcbdb
Sandra13988 <sandra13988@gmail.com>
2025-02-13 09:46:11 +0100

    feat(root): initialize components

---
+++ src/app/components/lenguage/lenguage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lenguage',
+  standalone: true,
+  imports: [],
+  templateUrl: './lenguage.component.html',
+  styleUrl: './lenguage.component.css'
+})
+export class LenguageComponent {
+
+}
